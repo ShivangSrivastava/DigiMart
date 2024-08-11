fa31ed990d2822547b55ba09a58c9f3330a3ea83
ShivangSrivastava <shivangsrivastava157@gmail.com>
2024-08-11 08:20:11 +0530

    "Refactor auth models, add floor and shared_preferences dependencies, and update pubspec.lock"

--- client/lib/features/auth/data/data_sources_export.dart
+++ client/lib/features/auth/data/data_sources_export.dart
@@ -1,3 +1,3 @@
 export 'package:client/features/auth/data/data_sources/auth_api_service.dart';
-export 'package:client/features/auth/data/models/auth_model.dart';
-export 'package:client/features/auth/data/models/auth_response_model.dart';
+export 'package:client/features/auth/data/models/remote/auth_model.dart';
+export 'package:client/features/auth/data/models/remote/auth_response_model.dart';
